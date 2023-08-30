9a7e4a605f79b49c2a3f18be3e6be3b83294d3e6
Sharmista Shastry <sharmistashastry@Sharmistas-MacBook-Air.local>
2023-08-30 19:30:25 -0400

    Adding my contribution to the project

--- include.js
+++ include.js
@@ -6526,6 +6526,13 @@ let cards = [
     author: 'Marion',
     githubLink: 'https://github.com/marionjudy13'
   },
+  {
+    artName: 'Typing Effect',
+    pageLink: './Art/sharmista/index.html',
+    imageLink: './Art/sharmista/typingEffect.gif',
+    author: 'Sharmista',
+    githubLink: 'https://github.com/sharmista2shastry'
+  },
 ];
 
 // +--------------------------------------------------------------------------------+
